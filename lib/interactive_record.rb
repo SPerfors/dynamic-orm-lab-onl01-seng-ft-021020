6f4528c1f80997b76edd50753e3c31bae2ce1402
Steven Perfors <stevecanbuild@gmail.com>
2020-04-03 18:14:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,7 +26,9 @@ class InteractiveRecord
     end 
   end
   
-  def 
+  def table_name_for_insert
+    self.class.table_name 
+  end
   
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(" , ")
